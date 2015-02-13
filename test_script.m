c3d11b3e801c431c1cb93cb439ded33355f2e5e7
Niklas Sallinen <niklas.sallinen@gmail.com>
2015-02-13 16:06:31 +0200

    Changed the coefficients of the phasor in harmoniser function.

--- test_script.m
+++ test_script.m
@@ -2,13 +2,13 @@
 fs = 44100;
 
 sound = audioread('Track22.wav');
-clip = sound(1:fs*8.6,:);
+clip = sound(20000:fs*3,:);
 clip = clip./max(abs(clip));
 s = size(clip);
 clip = clip.*tukeywin(s(1,1),0.05);
 
-output = harmonizer(clip, fs);
+output = WCHarmoniser(clip, fs);
 output = output./max(abs(output));
 
-out = clip + output;
+out = [clip; output];
 soundsc(out, fs)
