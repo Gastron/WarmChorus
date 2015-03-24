1a56bfb222fd87a50ddd7b12c26731b42e36c813
Sallinen Niklas <sallinn1@hjelmman.org.aalto.fi>
2015-03-24 13:21:16 +0200

    cleand the code of the WCHarmonizer.m

--- WCHarmoniser.m
+++ WCHarmoniser.m
@@ -1,26 +1,27 @@
 function [ y ] = WCHarmoniser( x, Fs, detune )
 
-%The Harmoniser for the Warm Chorus
-%	x: input
-%	y: output
-%	Fs: sampling frequency
+% The Harmoniser for the Warm Chorus
+% This function detunes the input signal. 
+%
+%	x:      input
+%   detune: amount of detuning and window size
+%	y:      output
+%	Fs:     sampling frequency
 
 %% generating phasor signals:
 
-f = 0.1*(0.1+detune); % frequency of phasor function
+f = 0.1*(0.2+detune);   % frequency of phasor function
 p = zeros(length(x),4); % empty matrix for phasor signals
 
 % constans to help to generate phasor signal:
-T = length(transpose(x))/Fs;
-dt = 1/Fs;
-t = 0:dt:T-dt;
-coef = 200*(0.7+detune);
-r = zeros(4,1);
+t = 0:1/Fs:(length(x)-1)/Fs;    % time vector for the phasor
+coef = 200*(0.7+detune);        % maximum modulation depth
 
+r = zeros(4,1);                 % initializing random numbers
 % genarating sawtooth signals for phasor:
 for k = 1:4
     p(:,k) =coef*(1-sawtooth(2*pi*f*t+(k-1).*pi/2))/2;
-    r(k) = 0.5+0.5*abs(rand(1)); % random number to get randomization to transpose
+    r(k) = 0.5+0.5*abs(rand(1)); % randomization for the transpose
 end;
 
 
@@ -28,10 +29,10 @@ end;
 %% initializing output and helping variables:
 
 
-y_k = zeros(length(x),4);  % output for different phasors initialized
-y = zeros(size(x)); % output sequence
-lst = zeros(4,1);   % additional variable to localize the place of the last window
-%d = 0;  % delay of the signal initialized to zero
+y_k = zeros(length(x),4);  % output for each phasor
+y = zeros(size(x));        % function output sequence
+lst = zeros(4,1);          % additional variable to localize 
+                           % the place of the last window
 
 %% creating the window for windowing:
 
@@ -39,8 +40,7 @@ winlen = round(Fs/f)+1; % window length
 w = hann(winlen); % Hanning window
 
 
-
-%% transposing the signal and windowing:
+%% transposing the signal:
 
 for n = 2:length(x)
     for k = 1:4
@@ -48,7 +48,7 @@ for n = 2:length(x)
         % modulated delay line to transpose the signal:
         if n-floor(p(n,k)*r(k)) >= 2
             
-            d = p(n,k)*r(k);     % delay
+            d = p(n,k)*r(k);  % delay line
             d_int = floor(d); % integer part of delay
             d_frac = d-d_int; % fractional part of delay 
             
@@ -60,12 +60,12 @@ for n = 2:length(x)
         % windowing:
         if abs(p(n,k) - p(n-1,k)) > coef*0.8 % finding the peaks of phasor
             
-            r(k) =  0.5+0.5*abs(rand(1)); % new random for every window
+            r(k) =  0.5+0.5*abs(rand(1)); % new random value for each window
             
             if n - winlen > 0 % checking if full window fits
                 y_k(n-winlen:n-1,k) = y_k(n-winlen:n-1,k).*w;
                 
-            else % adding part of the window if not
+            else % adding part of the window if not (first window)
                 y_k(1:n-1,k) = y_k(1:n-1,k).*w(winlen-n+2:end);
             end;
             
@@ -75,7 +75,6 @@ for n = 2:length(x)
             end;
         end;
     end;
-    %d=0; % delay to zero after each loop of phasors
 end;
 
 % the last window:
