cfa716c633150949fa115a15d7e29fdf1fbd06e1
Aku Rouhe <aku.rouhe@aalto.fi>
2015-03-27 15:23:41 +0200

    Finished the copyright clause and wrote a little about theory

--- EXAMPLE.m
+++ EXAMPLE.m
@@ -2,31 +2,29 @@
 %
 % Aku Rouhe & Niklas Sallinen
 
-
-%% reading input files:
-
+%%Reading input files:
 [gtr_lead, Fs1] = audioread('SoundSamples/GuitarLeadDRY.wav');
 [gtr_crd, Fs2] = audioread('SoundSamples/GuitarChordsDRY.wav');
 [gtr_dst, Fs3] = audioread('SoundSamples/GuitarDistortedDRY.wav');
 [violin, Fs4] = audioread('SoundSamples/DRYSAMPLE.wav');
 [music, Fs5] = audioread('SoundSamples/musicDRY.wav');
 
-
-%% Processing:
-
+%%Parameter values
+% typical values:
 distance = 5;   % Maximum distance between players
 skill    = 0.5; % Skill factor of players
+% horrible orchestra:
+horrible_distance = 50;
+horrible_skill = 0.1;
 
-% Process with WarmChorus
+%%Processing
+% Process with WarmChorus:
 violin_warm = WarmChorus(violin, Fs4, distance, skill);
-% Process with BasicChorus
+% Process with BasicChorus for comparison:
 violin_basic = BasicChorus(violin, Fs4);
 
-% horrible orchestra:
-% distance = 50;
-% skill = 0.1;
-% violin_horrible = WarmChorus(violin, Fs4, distance, skill);
+violin_horrible = WarmChorus(violin, Fs4, horrible_distance, horrible_skill);
 
 %% Uncomment to listen the examples:
-% soundsc([violin; violin_basic; violin_warm], Fs4);
+% soundsc([violin; violin_basic; violin_warm], Fs4); %First the dry, then the generic and finally Warm Chorus.
 % soundsc(violin_horrible, Fs4);
